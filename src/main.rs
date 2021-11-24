7d7bc5d6243be9922976da4751e43af3dc7ad225
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2021-11-24 05:04:44 +0000

    Very basic multi-threaded Piper server
    Needs some kind of pooling to limit the amount of threads.
    Needs to handle the different response types properly.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+use std::net::{TcpListener, TcpStream, SocketAddr};
+use std::io::prelude::*;
 use std::io;
+use std::thread;
 use piper::{FromReq, AsResp};
 
 mod piper;
 
 fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:60")?;
+    for stream in listener.incoming() {
+        thread::spawn(move ||{
+            handle_client(stream.unwrap());
+        });
+    }
 
     Ok(())
+}
+
+fn handle_client(mut stream: TcpStream){
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let req = piper::Request::read(&buffer);
+    let response = piper::TextResponse(format!("Hello World\nRequest: {:?}", req));
+    stream.write(&response.bytes()).unwrap();
+    stream.flush().unwrap();
+    println!("{:?}", req);
 }
